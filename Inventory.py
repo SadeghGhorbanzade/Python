3b65d4e6ecbdf93a1b88811827c603067011d92f
SquidForAQuid <oathbreaker.drw@gmail.com>
2014-02-14 20:09:54 +0000

    Deleting a few unwanted things

--- Inventory.py
+++
@@ -1,9 +0,0 @@
-class item(itemID, itemName, price, stock):
-    def __init__(self):
-        self.id = itemID
-        self.name = itemName
-        self.price = price
-        self.stock = stock
-
-class inventory:
-    return null
